586f884f2ca866bc67659a41fdc6529da91371ce
thompsco <thompsco@tcd.ie>
2019-11-25 20:02:21 +0000

    learning how to use the pygal library

--- GithubAccess/githubAccess.py
+++ GithubAccess/githubAccess.py
@@ -2,29 +2,21 @@ from github import Github
 from datetime import datetime
 import json
 import requests
+import pygal
 
 # using username and password
-g = Github("f1fa8afa56abeab676fd93490c39cccd56758d82")
+g = Github("cc2d7ccf0f53669bec8d83b1cbac2683d543b534")
 
 #for repo in g.get_user().get_repos():
 #    branch = repo.get_branch("master")
 #    print(str(repo), "        ", str(branch.commit))
 
 
-#public_repos = requests.get('https://api.github.com/repositories').json()
-#for repo in public_repos:
-#    repo_name = repo['name']
-#    owner = repo['owner']['login']
-#    repo_info = requests.get('https://api.github.com/repos/'+owner+'/'+repo_name)
-#    stars = repo_info.json()['stargazers_count']
-#    print(repo_name, stars)
-
-
 java_repos = g.search_repositories(query='language:Java')
 count = 0
 for repo in java_repos:
     if count < 10:
-        print(repo.stargazers_count, "      JAVA", str(repo))
+        print("JAVA", repo.stargazers_count, str(repo))
         count += 1
     else:
         break
@@ -35,7 +27,11 @@ python_repos = g.search_repositories(query='language:python')
 count = 0
 for repo in python_repos:
     if count < 10:
-        print(repo.stargazers_count, "      PYTHON", str(repo))
+        print("PYTHON", repo.stargazers_count, str(repo))
         count += 1
     else:
         break
+
+barchart = pygal.Bar()
+barchart.add('Fibonacci', [0, 1, 1, 2, 3, 5, 8, 13])
+barchart.render_to_file('barchart.svg')
