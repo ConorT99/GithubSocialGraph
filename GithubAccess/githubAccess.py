55da2708c5ea7283edf10db185214d1b049fdd32
thompsco <thompsco@tcd.ie>
2019-11-25 18:10:57 +0000

    Receiving 10 Java repos with the highest stargazers count
    Getting the 10 most starred public Java repos on GitHub

--- GithubAccess/githubAccess.py
+++ GithubAccess/githubAccess.py
@@ -1,8 +1,28 @@
 from github import Github
+from datetime import datetime
+import json
+import requests
 
 # using username and password
-g = Github("ConorT99", "redacted")
+g = Github("2b5aca630caa6f0de36cd40f318a4c404845e307")
 
 for repo in g.get_user().get_repos():
     branch = repo.get_branch("master")
     print(str(repo), "        ", str(branch.commit))
+
+
+#public_repos = requests.get('https://api.github.com/repositories').json()
+#for repo in public_repos:
+#    repo_name = repo['name']
+#    owner = repo['owner']['login']
+#    repo_info = requests.get('https://api.github.com/repos/'+owner+'/'+repo_name)
+#    stars = repo_info.json()['stargazers_count']
+#    print(repo_name, stars)
+
+
+java_repos = g.search_repositories(query='language:Java')
+count = 0
+for repo in java_repos:
+    if count < 10:
+        print(repo.stargazers_count)
+        count += 1
