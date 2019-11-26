1df8bc8277d343d623500f7b46e2512436b96d2f
thompsco <thompsco@tcd.ie>
2019-11-26 19:54:02 +0000

    I made a barchart to visualise the ratio between the 10 highest starred Java repos and a Pie Chart to do the same with the 10 python repos with the most stars

--- GithubAccess/githubAccess.py
+++ GithubAccess/githubAccess.py
@@ -5,33 +5,63 @@ import requests
 import pygal
 
 # using username and password
-g = Github("cc2d7ccf0f53669bec8d83b1cbac2683d543b534")
+g = Github("")
 
 #for repo in g.get_user().get_repos():
 #    branch = repo.get_branch("master")
 #    print(str(repo), "        ", str(branch.commit))
+java_stars = []
+java_repo_names = []
 
-
+# -- GATHERING 10 Most STARRED JAVA REPOS -- #
 java_repos = g.search_repositories(query='language:Java')
 count = 0
 for repo in java_repos:
     if count < 10:
-        print("JAVA", repo.stargazers_count, str(repo))
+        #print("JAVA", repo.stargazers_count, str(repo))
+        java_stars.insert(count, repo.stargazers_count)
+        java_repo_names.insert(count, repo.name)
         count += 1
     else:
         break
 
-print()
+# -- END GATHERING JAVA INFORMATION -- #
+
+python_stars = []
+python_repo_names = []
+
+# -- GATHERING 10 MOST STARRED PYTHON REPOS -- #
 
 python_repos = g.search_repositories(query='language:python')
 count = 0
 for repo in python_repos:
     if count < 10:
-        print("PYTHON", repo.stargazers_count, str(repo))
+        #print("PYTHON", repo.stargazers_count, str(repo))
+        python_stars.insert(count, repo.stargazers_count)
+        python_repo_names.insert(count, repo.name)
         count += 1
     else:
         break
 
-barchart = pygal.Bar()
-barchart.add('Fibonacci', [0, 1, 1, 2, 3, 5, 8, 13])
-barchart.render_to_file('barchart.svg')
+# -- END GATHERING PYTHON INFORMATION -- #
+
+pystars_total = 0
+for i in range(0, len(python_stars)):
+    pystars_total += python_stars[i]
+print("Python repo stars (in toto): " + str(pystars_total))
+
+py_share_of_total = []
+for i in range(0, len(python_stars)):
+    py_share_of_total.insert(i, (python_stars[i]/pystars_total) * 100)
+
+print(py_share_of_total)
+
+java_bars = pygal.Bar()
+for i in range(0, len(java_repo_names)):
+    java_bars.add(java_repo_names[i], java_stars[i])
+java_bars.render_to_file('javaBarchart.svg')
+
+pypie = pygal.Pie()
+for i in range(0, len(python_repo_names)):
+    pypie.add(python_repo_names[i], py_share_of_total[i])
+pypie.render_to_file('pyPie.svg')
